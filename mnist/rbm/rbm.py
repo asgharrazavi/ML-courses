fdbbab353046a2c77529a8ec22f466dfb5eb4590
Asghar Razavi <asghar.razavi@gmail.com>
2018-11-02 13:54:44 -0400

    rbm.py

--- mnist/rbm/rbm.py
+++ mnist/rbm/rbm.py
@@ -11,6 +11,9 @@ def rbm(X,n_inp,n_hid,last_RBM=False):
     w: flaot, weight matrix, 2d array, shape=(n_inp,n_hid)
     v: float, visable layer biases, 2d array, shape=(1,n_inp)
     h: float, hidden layer biases, 2d array, shape=(1,n_hid) 
+
+    Notes:
+    -----
     """
 
     print "data.shape:", X.shape
